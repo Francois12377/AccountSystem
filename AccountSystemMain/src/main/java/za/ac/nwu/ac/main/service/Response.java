462a68036b00f84d3113213f8995fe8c08b1b3e2
Francois12377 <francoiskruger55@gmail.com>
2021-09-26 20:19:57 +0200

    Added Swagger, Bean conflict in AccountTypeController

--- AccountSystemMain/src/main/java/za/ac/nwu/ac/main/service/Response.java
+++ AccountSystemMain/src/main/java/za/ac/nwu/ac/main/service/Response.java
@@ -1,6 +1,6 @@
 package za.ac.nwu.ac.main.service;
 
 public class Response<S> {
-    public Response(boolean successful, S no_types_found) {
+    public Response(boolean successful, String payload) {
     }
 }
--- AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/Controller/AccountTypeController.java
+++ AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/Controller/AccountTypeController.java
@@ -4,9 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.nwu.ac.main.service.Response;
 
-@RestController
+@RestController(value = "BeanName")
 public class AccountTypeController {
-
     @GetMapping("/all")
-    public Response<String> getAll() {return new Response<> (true, "No Types found");}
+    public Response<String> getAll() {return new Response<>(true, "No Types found");}
 }
