3afbd1ab8eb64d3365e2fb16a099918c61654c6e
Francois12377 <francoiskruger55@gmail.com>
2021-10-01 21:08:45 +0200

    Bean conflict in AccountTypeController Still there

--- AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/Controller/AccountTypeControl.java
+++ AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/Controller/AccountTypeControl.java
@@ -4,7 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.nwu.ac.main.service.Response;
 
-@RestController
+
+
+@RestController("BeanName")
 public class AccountTypeControl {
     @GetMapping("/all")
     public Response<String> getAll() {return new Response<>(true, "No Types found");}
