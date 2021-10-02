ec00f30c72c33b8f9b7e9f363bd095f92a3f8aae
Francois12377 <francoiskruger55@gmail.com>
2021-10-02 16:16:10 +0200

    Changed name to try and fix bean conflict(didn't work), Also added mapping request

--- AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/Controller/AccountTypeControl.java
+++ AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/Controller/AccountTypeControl.java
@@ -1,13 +1,15 @@
 package za.ac.nwu.ac.web.sb.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import za.ac.nwu.ac.main.service.Response;
+import za.ac.nwu.ac.main.service.GeneralResponse;
 
 
 
 @RestController("BeanName")
+@RequestMapping("Demo")
 public class AccountTypeControl {
     @GetMapping("/all")
-    public Response<String> getAll() {return new Response<>(true, "No Types found");}
+    public GeneralResponse<String> getAll() {return new GeneralResponse<>(true, "No Types found");}
 }
