f66495a406a9452b7c94055417a66c1fb03e74a1
Francois12377 <francoiskruger55@gmail.com>
2021-10-02 16:20:21 +0200

    Tried fixing Bean error, didn't work

---
+++ AccountSystemMain/src/main/java/za/ac/nwu/ac/main/service/GeneralResponse.java
@@ -0,0 +1,6 @@
+package za.ac.nwu.ac.main.service;
+
+public class GeneralResponse<S> {
+    public GeneralResponse(boolean successful, String payload) {
+    }
+}
--- AccountSystemMain/src/main/java/za/ac/nwu/ac/main/service/Response.java
+++
@@ -1,6 +0,0 @@
-package za.ac.nwu.ac.main.service;
-
-public class Response<S> {
-    public Response(boolean successful, String payload) {
-    }
-}
