6aa15f03e355252bbef486448c55ec3360ac1a13
Francois12377 <francoiskruger55@gmail.com>
2021-10-07 12:00:33 +0200

    GeneralResponse Class got an upgrade

--- AccountSystemMain/src/main/java/za/ac/nwu/ac/main/service/GeneralResponse.java
+++ AccountSystemMain/src/main/java/za/ac/nwu/ac/main/service/GeneralResponse.java
@@ -1,24 +1,46 @@
 package za.ac.nwu.ac.main.service;
 
-public class GeneralResponse<S> {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class GeneralResponse<S>  implements Serializable {
     private final boolean successful;
     //private final transient S payload;
     private final String payload;
 
 
-
-    public GeneralResponse(boolean successful, S payload)
-    {
-        this.successful = true;
-        System.out.println("/n");
-        this.payload = "No Types Found";
-
+    public GeneralResponse(boolean successful, String payload) {
+        this.successful = successful;
+        this.payload = payload;
     }
 
-    public boolean getSuccessful() {
+
+    public boolean isSuccessful() {
         return successful;
     }
+
     public String getPayload() {
         return payload;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeneralResponse<?> that = (GeneralResponse<?>) o;
+        return successful == that.successful && Objects.equals(payload, that.payload);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(successful, payload);
+    }
+
+    @Override
+    public String toString() {
+        return "GeneralResponse{" +
+                "successful=" + successful +
+                ", payload='" + payload + '\'' +
+                '}';
     }
 }
